bd8c40359402578cb19435c5d4e2d8e5e0a87131
Angel <okparaangel@gmail.com>
2024-02-02 11:08:26 +0100

    i added a read file

--- pyth1.py
+++ pyth1.py
@@ -7,3 +7,15 @@ if c_d:
         print('string exist in a file')
 else:
         print('string does not exist in a file')  
+
+file_name = "prj1.txt"  
+with open(file_name, 'a+') as file:  
+        file.write(f"{name} \n")
+        file.write(f"{c_d} \n")
+        file.write(f"{c_t} \n")
+
+with open(file_name, 'r') as file:
+        print("\nContentents of the file: ")
+        for line in file:
+                print(line.strip())
+       
