333ce6eab9512ebd1f94a455dfce027b68d6c77b
Angel <okparaangel@gmail.com>
2024-02-02 11:06:12 +0100

    i added a search file

---
+++ pyth1.py
@@ -0,0 +1,9 @@
+def search_str(project1,word):
+        with open(project1.py, 'r') as file:
+                #read all content of a content of a file
+                content = file.read()            
+#check if string present in a file
+if c_d:
+        print('string exist in a file')
+else:
+        print('string does not exist in a file')  
